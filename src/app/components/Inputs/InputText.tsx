4e953424c1fd0a079af995f6fdf12e6e9a943e96
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-11-13 10:25:53 -0300

    Loign

---
+++ src/app/components/Inputs/InputText.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useEffect } from 'react';
+
+type InputTextProps = {
+  campo: string;
+  placeholder?: string;
+  type: string;
+  nombre: string;
+  register: any;
+  setValue?: any;
+  errors: any;
+  require?: boolean;
+  valor?: string;
+};
+
+// Definimos el componente funcional InputText
+export default function InputText({  campo, placeholder, type, nombre, register, require, setValue, errors, valor }: InputTextProps) {
+
+  useEffect(() => {
+    if (valor) {
+      setValue(nombre, valor);
+    }
+  }, [setValue, nombre, valor, errors]);
+
+
+  return (
+    <>
+    {
+      <label className={"block font-semibold"}>
+        {campo}
+      </label>
+      }
+       {errors && <span className="mt-1 text-red-600">{placeholder || campo} es requerido</span>} 
+      <input
+        type={type}
+        className={`block text-black w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-900`}
+        {...register(nombre, { required: require === true ? true : false })} placeholder={placeholder}
+      />
+    </>
+  );
+}
+
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,32 @@
+"use client"
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import InputText from '../components/Inputs/InputText';
+import Link from 'next/link';
 
 function page() {
+
+  const { register, handleSubmit, formState: { errors }, setValue } = useForm();
+
   return (
-    <div>page</div>
+    <div className='h-screen flex flex-col items-center justify-center bg-green-500'>
+      <div className='w-full md:w-3/10 h-screen md:h-7/10 flex flex-col items-center justify-center bg-white rounded-lg p-2'> 
+        <h1 className='text-4xl'>Inicia sesión</h1>
+        <form className='w-full p-2' 
+        onSubmit={
+        handleSubmit((data) => {
+          console.log(data);
+        })
+        }>
+        <InputText campo="Nombre de usuario"  type="text" nombre="nombre_de_usuario" register={register} require errors={errors.nombre_de_usuario} />
+        <InputText campo="Contraseña"  type="password" nombre="pass" register={register} require errors={errors.pass} />
+        <div className='flex items-center justify-center'>
+        <span>¿No tienes cuenta? <Link href='/register' className='text-green-600'>¡Registrate!</Link></span>
+        </div>
+        <button type="submit" className='bg-green-600 text-white p-2 rounded-lg w-full my-2 '>Iniciar sesión</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
