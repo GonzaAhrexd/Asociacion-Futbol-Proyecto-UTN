5795fcce00cad8faab04c6cd49ebc96192e05e84
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-11-13 09:29:31 -0300

    NavBar y pequeña estructura

---
+++ src/app/components/NavBar/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function navBar() {
+  return (
+    <div 
+    className='bg-green-600 flex flex-col items-center justify-center w-full'
+    > 
+      <p className='text-bold flex flex-col items-center justify-center text-white text-xl md:text-4xl '>
+      Asociación de Fútbol Resistencia
+      </p>
+    </div>
+  )
+}
+
+export default navBar
