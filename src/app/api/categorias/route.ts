669d63da78c051480d25a3a2e4504d603b9555ca
santi <santiezapata.2@gmail.com>
2024-11-24 16:19:02 -0300

    Creacion de get y post de categorias

---
+++ src/app/api/categorias/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+// Método GET: Obtiene todas las categorías de la base de datos
+export async function GET() {
+  try {
+    const categorias = await prisma.categoria.findMany();
+    return NextResponse.json(categorias);
+  } catch  {
+    return NextResponse.json({ error: 'Error al obtener categorías' }, { status: 500 });
+  }
+}
+
+// Método POST: Crea una nueva categoría en la base de datos
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { nombre, edad_minima, edad_maxima } = body;
+
+    // // Validación básica de datos
+    // if (!nombre || typeof edad_minima !== 'number' || typeof edad_maxima !== 'number') {
+    //   return NextResponse.json(
+    //     { error: 'Datos inválidos: nombre, edad_minima y edad_maxima son requeridos' },
+    //     { status: 400 }
+    //   );
+    // }
+
+    const nuevaCategoria = await prisma.categoria.create({
+      data: { nombre, edad_minima, edad_maxima},
+    });
+
+    return NextResponse.json(nuevaCategoria, { status: 201 });
+  } catch  {
+    return NextResponse.json({ error: 'Error al crear la categoría' }, { status: 500 });
+  }
+}
