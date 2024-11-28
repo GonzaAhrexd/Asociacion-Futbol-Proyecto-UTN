8b22b4b230fda1df1c96d94b859ee3323050d82c
santi <santiezapata.2@gmail.com>
2024-11-28 17:25:20 -0300

    Actualizacion de la vista cageorias

--- src/app/categorias/page.tsx
+++ src/app/categorias/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { obtenerCategorias, eliminarCategoria, crearCategoria } from '../api/services/categorias';
-import Link from 'next/link';
+import { obtenerCategorias, crearCategoria } from '../api/services/categorias';
 import DataTable from 'react-data-table-component';
 import ColumnCategorias from './columnCategorias';
 import expandedComponents from './expandedCategoria';
@@ -10,131 +9,141 @@ import { useForm } from 'react-hook-form';
 import Swal from 'sweetalert2';
 import InputText from '@/components/Inputs/InputText';
 import InputNumber from '@/components/Inputs/InputNumber';
+
 interface Categoria {
   nombre: string;
   edad_minima: number;
   edad_maxima: number;
 }
 
-const ListaCategorias = () => {
-  
+export default function ListaCategorias(){
+
   const [categorias, setCategorias] = useState<Categoria[]>([]);
   const [vistaAgregar, setVistaAgregar] = useState(false);
 
-  const {register, handleSubmit, setValue, formState: { errors } } = useForm();
+  const { register, handleSubmit, setValue, formState: { errors } } = useForm();
+
   useEffect(() => {
     const fetchCategorias = async () => {
-      const data = await obtenerCategorias();
-      setCategorias(data);
+      const categorias = await obtenerCategorias();
+      setCategorias(categorias);
     };
 
     fetchCategorias();
   }, []);
 
-  return (
-    <div className='p-5'>
+  function cancelarEnvio(){
+    setVistaAgregar(false)
+  }
 
-      <div className='flex'>
-        <h1 className='text-3xl mr-2'>Listado de categorías</h1>
-        <button  className='bg-blue-200 px-4 py-2 rounded-xl' onClick={() => setVistaAgregar(!vistaAgregar)}>
-          Agregar
-        </button>
-      </div>
+  const onSubmit = async (values: any) => {
+    Swal.fire({
+      icon: 'warning',
+      title: '¿Estás seguro de que deseas crear la categoría?',
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Crear',
+      confirmButtonColor: '#22C55E',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        console.log(values);
+        values.edad_minima = Number(values.edad_minima);
+        values.edad_maxima = Number(values.edad_maxima);
 
-    { vistaAgregar && 
-    <div className='flex items-center justify-center'>
-        <form className="flex flex-col p-5 bg-gray-100 shadow m-2 w-[400px]" 
-          onSubmit={
-            handleSubmit(async (values ) => {
-              Swal.fire({
-                icon: 'warning',
-                title: '¿Estás seguro de que deseas editar la categoría?',
-                showCancelButton: true,
-                confirmButtonText: `Editar`,
-                cancelButtonText: `Cancelar`,
-                confirmButtonColor: '#22C55E',
-              }).then(async (result) => {
-                if (result.isConfirmed) {
-                  console.log(values)
-                  
-                  values.edad_minima = Number(values.edad_minima)
-                  values.edad_maxima = Number(values.edad_maxima)
-
-                  // @ts-ignore
-                  await crearCategoria(values);
-                  Swal.fire({
-                    icon: 'success',
-                    title: 'Categoría agregada',
-                    showConfirmButton: false,
-                    timer: 1500
-                  }).then(() => {
-                    window.location.reload();
-                  })
-
-                }
-              })
-          }
-          )
-        }
-        >
-          <InputText
-            campo="Nombre"
-            // valor={data.nombre}
-            type="text"
-            nombre="nombre"
-            register={register}
-            setValue={setValue}
-            errors={errors.nombre}
-          />
-          <div className='flex'>
-            <InputNumber
-              campo="Edad mínima"
-              // valor={data.edad_minima}
-              nombre="edad_minima"
-              register={register}
-              setValue={setValue}
-              error={errors.edad_minima}
-              type="number"
-              maxLenght={3}
-            />
-            <InputNumber
-              campo="Edad máxima"
-              // valor={data.edad_maxima}
-              nombre="edad_maxima"
-              register={register}
-              setValue={setValue}
-              error={errors.edad_maxima}
-              type="number"
-              maxLenght={3}
-            />
-          </div>
-
-          <button className=" mr-2 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full my-2">
-              Enviar
-          </button>
-          
-
-
-        </form>
-      </div>
+        await crearCategoria(values);
+        Swal.fire({
+          icon: 'success',
+          title: 'Categoría agregada',
+          showConfirmButton: false,
+          timer: 1500,
+        }).then(() => {
+          window.location.reload();
+        });
       }
+    });
+  };
 
-      <DataTable
-        columns={ColumnCategorias}
-        data={categorias}
-        pagination
-        expandableRows
-        expandableRowsComponent={expandedComponents}
-        responsive={true}
-        striped={true}
-        highlightOnHover={true}
-        noDataComponent="No hay categorías para mostrar"
-        defaultSortFieldId={"Fecha"}
-      />
+  return (
 
-    
+    <div className=''> 
+      <div className="p-4">
+
+        <div className="flex my-10">
+          <h1 className="text-3xl mr-2">Listado de categorías</h1>
+          <button
+            className="bg-blue-200 px-4 py-2 rounded-xl"
+            onClick={() => setVistaAgregar(!vistaAgregar)}
+          >
+            Agregar
+          </button>
+        </div>
+
+        {vistaAgregar && (
+          <div className="flex items-center justify-center">
+            <form
+              className="flex flex-col p-5 bg-gray-100 shadow m-2 w-[400px]"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <InputText
+                campo="Nombre"
+                type="text"
+                nombre="nombre"
+                register={register}
+                setValue={setValue}
+                errors={errors.nombre}
+              />
+              <div className="flex my-2">
+                <InputNumber
+                  campo="Edad mínima"
+                  nombre="edad_minima"
+                  register={register}
+                  setValue={setValue}
+                  error={errors.edad_minima}
+                  type="number"
+                  maxLenght={3}
+                />
+                <InputNumber
+                  campo="Edad máxima"
+                  nombre="edad_maxima"
+                  register={register}
+                  setValue={setValue}
+                  error={errors.edad_maxima}
+                  type="number"
+                  maxLenght={3}
+                />
+              </div>
+
+              <div className='flex'>
+                <button className="mr-2 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full my-2">
+                  Crear
+                </button>
+                <button className="mr-2 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded w-full my-2" onClick={cancelarEnvio}>
+                  Cancelar
+                </button>
+              </div>
+
+              
+            </form>
+          </div>
+        )}
+
+        <DataTable
+          columns={ColumnCategorias}
+          data={categorias}
+          pagination
+          expandableRows
+          expandableRowsComponent={expandedComponents}
+          responsive
+          striped
+          highlightOnHover
+          noDataComponent="No hay categorías para mostrar"
+          defaultSortFieldId="Fecha"
+        />
+        </div>
+
     </div>
+    
   );
 };
 
-export default ListaCategorias;
+
