9310d55078c4a67bcedae1cffdd5a025e1f48ca9
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2024-11-28 12:00:50 -0300

    Solucion de errores

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,33 +1,42 @@
 "use client";
+
 import React from "react";
 import Link from "next/link";
-import { useSession, signOut } from "next-auth/react";
 
-function navBar() {
-  const estilosBotones = "bg-green-200 px-4 py-2 rounded-xl mr-2";
-
-  const botonGris300="bg-green-300 px-4 py-2 rounded-xl mr-2"
-  const botonGris400="bg-green-400 px-4 py-2 rounded-xl mr-2"
+function NavBar() {
+  const botonGris300 = "bg-green-300 px-4 py-2 rounded-xl mr-2";
+  const botonGris400 = "bg-green-400 px-4 py-2 rounded-xl mr-2";
 
   return (
     <div className="bg-green-400 flex flex-row items-center justify-center w-full p-1">
-    <div className='bg-green-200 flex flex-row items-center justify-center w-full p-2'> 
-
-      {/* <p className='text-bold flex flex-col items-center justify-center text-white text-xl md:text-4xl '>
-          Asociación de Fútbol Resistencia
-      </p> */}
-
-      <Link href="/" className={botonGris300}>Inicio</Link>
-      <Link href="/equipos" className={botonGris300}>Equipos</Link>
-      <Link href="/torneos" className={botonGris300}>Torneos</Link>
-      <Link href="/personas" className={botonGris300}>Personas</Link>
-      <Link href="/" className={botonGris300}>Partidos</Link>
-      <Link href="/" className={botonGris300}>Estadisticas</Link>
-      <Link href="/login" className={botonGris400}>Iniciar sesion</Link>
-      <Link href="/register" className={botonGris400}>Registrarse</Link>
-
+      <div className="bg-green-200 flex flex-row items-center justify-center w-full p-2">
+        <Link href="/" className={botonGris300}>
+          Inicio
+        </Link>
+        <Link href="/equipos" className={botonGris300}>
+          Equipos
+        </Link>
+        <Link href="/torneos" className={botonGris300}>
+          Torneos
+        </Link>
+        <Link href="/personas" className={botonGris300}>
+          Personas
+        </Link>
+        <Link href="/" className={botonGris300}>
+          Partidos
+        </Link>
+        <Link href="/" className={botonGris300}>
+          Estadísticas
+        </Link>
+        <Link href="/login" className={botonGris400}>
+          Iniciar sesión
+        </Link>
+        <Link href="/register" className={botonGris400}>
+          Registrarse
+        </Link>
+      </div>
     </div>
   );
 }
 
-export default navBar;
+export default NavBar;
