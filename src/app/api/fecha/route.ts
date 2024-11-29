5b172ed2a1318d04eaad5a16142c1fc35814fa47
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2024-11-29 00:56:55 -0300

    Más rutas API

--- src/app/api/fecha/route.ts
+++ src/app/api/fecha/route.ts
@@ -6,9 +6,11 @@ const prisma = new PrismaClient();
 
 
 /*
+
 model Fecha {
-  nro_fecha Int @id // PK
+  codigo_fecha Int @id // PK
   cant_partidos Int
+  nro_fecha_rueda Int @default(0) // valor por defecto
 
   ruedas RuedaFecha[]
   partidos Partido[]
@@ -36,58 +38,58 @@ export async function GET(req: Request) {
     }
 }
 
-
-export async function POST(req: Request, res: any){
-    try{
+export async function POST(req: Request, res: any) {
+    try {
         // Obtener el cuerpo de la solicitud
         const body = await req.json();
 
         // Extraer los datos del cuerpo
-        const { cant_partidos } = body;
+        const { cant_partidos, nro_fecha_rueda } = body;
 
         // Insertar una nueva fecha en la base de datos
         const fecha = await prisma.$queryRaw`
-        INSERT INTO Fecha (cant_partidos)
-        VALUES (${Number(cant_partidos)})`;
+        INSERT INTO Fecha (cant_partidos, nro_fecha_rueda)
+        VALUES (${Number(cant_partidos)}, ${Number(nro_fecha_rueda)})`;
+
         return NextResponse.json("Fecha creada");
-        // Devolver la fecha creada
-    }catch(error){
-        console.log(error)
+    } catch (error) {
+        console.log(error);
         return handleError('Error al crear la fecha');
     }
 }
 
 export async function PUT(req: Request) {
-    try{
+    try {
         // Obtener los datos del body
         const body = await req.json();
-
-        const { nro_fecha, cant_partidos } = body;
+        const { codigo_fecha, cant_partidos, nro_fecha_rueda } = body;
 
         // Actualizar la fecha
         const fecha = await prisma.$queryRaw`
         UPDATE Fecha
-        SET cant_partidos = ${Number(cant_partidos)}
-        WHERE nro_fecha = ${Number(nro_fecha)}`;
+        SET cant_partidos = ${Number(cant_partidos)}, nro_fecha_rueda = ${Number(nro_fecha_rueda)}
+        WHERE codigo_fecha = ${Number(codigo_fecha)}`;
+
         return NextResponse.json("Fecha actualizada");
-    }catch(error){
-        console.log(error)
+    } catch (error) {
+        console.log(error);
         return handleError('Error al actualizar la fecha');
     }
 }
 
 export async function DELETE(req: Request) {
-    try{
-        // Obtener el nro_fecha de la fecha a eliminar
-        const { nro_fecha } = await req.json();
+    try {
+        // Obtener el codigo_fecha de la fecha a eliminar
+        const { codigo_fecha } = await req.json();
 
         // Eliminar la fecha
         const fecha = await prisma.$queryRaw`
         DELETE FROM Fecha
-        WHERE nro_fecha = ${Number(nro_fecha)}`;
+        WHERE codigo_fecha = ${Number(codigo_fecha)}`;
+
         return NextResponse.json("Fecha eliminada");
-    }catch(error){
-        console.log(error)
+    } catch (error) {
+        console.log(error);
         return handleError('Error al eliminar la fecha');
     }
 }
