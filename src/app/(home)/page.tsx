3eb92ae5df167db212ca010928046b0524838988
santi <santiezapata.2@gmail.com>
2024-11-29 13:15:19 -0300

    Solucion de key prop del inicio

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import BotonSecciones from "@/components/BotonSecciones/BotonSecciones";
+
 export default function Home() {
   const botonGris300 = "m-2 bg-gray-300 px-5 py-2 rounded";
 
@@ -24,8 +25,8 @@ export default function Home() {
     {
       nombre: "Torneos",
       ruta: "/torneos",
-    }
-  ]
+    },
+  ];
   const seccionesLogout = [
     {
       nombre: "Iniciar Sesion",
@@ -34,40 +35,38 @@ export default function Home() {
     {
       nombre: "Registrarse",
       ruta: "/register",
-    }]
+    },
+  ];
 
   return (
-    
-      <div className="flex flex-col items-center p-4">
-
-        <div className="my-8">
-          {user ? (
-            <>
-              <h1 className="ml-3 text-4xl m-auto">Bienvenido, {user.name}!</h1>
-            </>
-          ) : (
-            <>
-              <h1 className="ml-3 text-4xl m-auto">No estas logueado {':('}</h1>
-            </>
-          )}
-        </div>
-        <h1
-          className="text-3xl font-bold text-center text-neutral-900"
-        >Secciones</h1>
-        <div className='grid gap-1 grid-cols-1 sm:gap-5 md:grid-cols-4 lg:grid-cols-5 w-full '>
-        
-
-        
-          {user ? secciones.map((seccion) => (
-            <BotonSecciones seccion={seccion.nombre} ruta={seccion.ruta} />
-          ))
-          : seccionesLogout.map((seccion) => (
-            <BotonSecciones seccion={seccion.nombre} ruta={seccion.ruta} />
-          ))
-        }
-        
-        </div>
+    <div className="flex flex-col items-center p-4">
+      <div className="my-8">
+        {user ? (
+          <h1 className="ml-3 text-4xl m-auto">Bienvenido, {user.name}!</h1>
+        ) : (
+          <h1 className="ml-3 text-4xl m-auto">No estas logueado {":("}</h1>
+        )}
       </div>
-
+      <h1 className="text-3xl font-bold text-center text-neutral-900">
+        Secciones
+      </h1>
+      <div className="grid gap-1 grid-cols-1 sm:gap-5 md:grid-cols-4 lg:grid-cols-5 w-full">
+        {user
+          ? secciones.map((seccion) => (
+              <BotonSecciones
+                key={seccion.nombre}
+                seccion={seccion.nombre}
+                ruta={seccion.ruta}
+              />
+            ))
+          : seccionesLogout.map((seccion) => (
+              <BotonSecciones
+                key={seccion.nombre}
+                seccion={seccion.nombre}
+                ruta={seccion.ruta}
+              />
+            ))}
+      </div>
+    </div>
   );
 }
