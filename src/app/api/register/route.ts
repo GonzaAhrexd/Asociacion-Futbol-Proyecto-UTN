f1ff0b74e52c84e838fca13aee490ed475c4b2e4
ceso217 <baronicecilio@gmail.com>
2024-11-29 05:12:10 -0300

    creación jugador

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -9,93 +9,99 @@ const prisma = new PrismaClient();
 // }
 
 export async function POST(req: Request) {
-  const body = await req.json();
-  const {
-    nombre,
-    apellido,
-    dni,
-    fecha_nacimiento,
-    direccion_calle,
-    direccion_altura,
-    pass,
-    rol,
-    nro_socio,
-    foto,
-    nivel_exp,
-    tiene_cert,
-  } = body;
-  // Guardalo en Persona
+  try {
+    const body = await req.json();
+    const {
+      nombre,
+      apellido,
+      dni,
+      fecha_nacimiento,
+      direccion_calle,
+      direccion_altura,
+      pass,
+      // rol,
+      // nro_socio,
+      // foto,
+      // nivel_exp,
+      // tiene_cert,
+    } = body;
+    // Guardalo en Persona
 
-  const convertToDate = new Date(fecha_nacimiento);
+    console.log(body);
 
-  console.log(body);
+    // return NextResponse.json("Fecha actualizada");
 
-  const hashedPassword = await bcrypt.hash(pass, 10);
+    const convertToDate = new Date(fecha_nacimiento);
 
-  console.log(req.body);
+    const hashedPassword = await bcrypt.hash(pass, 10);
 
-  const nuevaPersona = await prisma.persona.create({
-    data: {
-      dni: Number(dni),
-      nombre: nombre + " " + apellido,
-      fecha_nacimiento: convertToDate,
-      direccion: direccion_calle + " " + direccion_altura,
-      pass: hashedPassword,
-      es_encargado: false,
-    },
-  });
+    console.log("Body recibido:", req.body);
 
-  function calcularEdad(fecha_nacimiento: Date): number {
-    const hoy = new Date(); // Fecha actual
-    const nacimiento = new Date(fecha_nacimiento); // Fecha de nacimiento
-
-    // Calcular la diferencia en años
-    let edad = hoy.getFullYear() - nacimiento.getFullYear();
-
-    // Ajustar la edad si el cumpleaños aún no ha ocurrido este año
-    const mesActual = hoy.getMonth();
-    const diaActual = hoy.getDate();
-    const mesNacimiento = nacimiento.getMonth();
-    const diaNacimiento = nacimiento.getDate();
-
-    // Si aún no ha llegado el cumpleaños de este año, resta 1
-    if (
-      mesActual < mesNacimiento ||
-      (mesActual === mesNacimiento && diaActual < diaNacimiento)
-    ) {
-      edad--;
-    }
-
-    return edad;
-  }
-
-  const edadJugador = calcularEdad(fecha_nacimiento);
-  let categoriaJugador = "";
-
-  if (edadJugador >= 41 && edadJugador <= 45) {
-    categoriaJugador = "Maxi";
-  } else if (edadJugador >= 46 && edadJugador <= 50) {
-    categoriaJugador = "Súper";
-  } else if (edadJugador >= 51 && edadJugador <= 55) {
-    categoriaJugador = "Máster";
-  } else {
-    categoriaJugador = "Categoría no existente";
-  }
-
-  if (rol == "Jugador") {
-    const nuevoJugador = await prisma.jugador.create({
+    const nuevaPersona = await prisma.persona.create({
       data: {
-        dni_jugador_fk: Number(dni),
-        nro_socio: nro_socio,
-        foto: foto,
-        categoria_fk: categoriaJugador,
+        dni: Number(dni),
+        nombre: nombre + " " + apellido,
+        fecha_nacimiento: convertToDate,
+        direccion: direccion_calle + " " + direccion_altura,
+        pass: hashedPassword,
+        es_encargado: false,
       },
     });
 
-    return NextResponse.json({ nuevaPersona, nuevoJugador }, { status: 201 });
+    return NextResponse.json(nuevaPersona, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error });
   }
 
-  return NextResponse.json(nuevaPersona, { status: 201 });
+  // function calcularEdad(fecha_nacimiento: Date): number {
+  //   const hoy = new Date(); // Fecha actual
+  //   const nacimiento = new Date(fecha_nacimiento); // Fecha de nacimiento
 
-  // return NextResponse.json({ message: 'Datos recibidos', data: body });
+  //   // Calcular la diferencia en años
+  //   let edad = hoy.getFullYear() - convertToDate.getFullYear();
+
+  //   // Ajustar la edad si el cumpleaños aún no ha ocurrido este año
+  //   const mesActual = hoy.getMonth();
+  //   const diaActual = hoy.getDate();
+  //   const mesNacimiento = nacimiento.getMonth();
+  //   const diaNacimiento = nacimiento.getDate();
+
+  //   // Si aún no ha llegado el cumpleaños de este año, resta 1
+  //   if (
+  //     mesActual < mesNacimiento ||
+  //     (mesActual === mesNacimiento && diaActual < diaNacimiento)
+  //   ) {
+  //     edad--;
+  //   }
+
+  //   return edad;
+  // }
+
+  // const edadJugador = calcularEdad(fecha_nacimiento);
+  // let categoriaJugador = "";
+
+  // if (edadJugador >= 41 && edadJugador <= 45) {
+  //   categoriaJugador = "Maxi";
+  // } else if (edadJugador >= 46 && edadJugador <= 50) {
+  //   categoriaJugador = "Super";
+  // } else if (edadJugador >= 51 && edadJugador <= 55) {
+  //   categoriaJugador = "Master";
+  // } else {
+  //   categoriaJugador = "Master";
+  // }
+
+  // if (rol == "Jugador") {
+  //   const nuevoJugador = await prisma.jugador.create({
+  //     data: {
+  //       dni_jugador_fk: Number(dni),
+  //       nro_socio: Number(nro_socio),
+  //       foto: "foto",
+  //       categoria_fk: categoriaJugador,
+  //     },
+  //   });
+
+  //   return NextResponse.json({ nuevaPersona, nuevoJugador }, { status: 201 });
+  // }
+
+  // return NextResponse.json({ message: "Datos recibidos", data: body });
 }
