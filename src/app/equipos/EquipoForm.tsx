773ef8bfc928efb447c6be2288caf138263db821
santi <santiezapata.2@gmail.com>
2024-11-29 02:05:44 -0300

    va yennndo el crud de equipos

---
+++ src/app/equipos/EquipoForm.tsx
@@ -0,0 +1,190 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  crearEquipo,
+  actualizarEquipo,
+  eliminarEquipo,
+} from "../../utils/equipos/equipos";
+import { useForm } from "react-hook-form";
+import InputText from "@/components/Inputs/InputText";
+
+interface Equipo {
+  nro_equipo?: number;
+  nombre?: string;
+  dni_dt_fk?: number;
+  categoria_fk?: string;
+  division?: string;
+}
+
+interface EquipoFormProps {
+  creando: boolean;
+  data?: Equipo;
+  onUpdate: () => void; // Actualizar lista de equipos
+}
+
+export default function EquipoForm({ creando, data, onUpdate }: EquipoFormProps) {
+  const { nombre = "", dni_dt_fk, categoria_fk, division } = data || {};
+  const [accionCrear, setAccionCrear] = useState(creando);
+
+  const listaDnis = ["1345", "12345678", "66666666"];
+  const listaDivisiones = ["A", "B", "C"];
+  const listaCategorias = ["juvenil", "Super", "Maxi"];
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (values: any) => {
+    try {
+      if (accionCrear) {
+        values.dni_dt_fk = Number(values.dni_dt_fk);
+        await crearEquipo(values);
+        console.log("Equipo creado");
+      } else {
+        values.nro_equipo = data?.nro_equipo;
+        await actualizarEquipo(values);
+        console.log("Equipo actualizado");
+      }
+      onUpdate(); // Actualizar listado
+    } catch (error) {
+      console.error("Error al guardar el equipo:", error);
+    }
+  };
+
+  const handleEliminar = async (nombre: string | undefined) => {
+    if (!nombre) {
+      alert("Error: No se puede eliminar un equipo sin nombre.");
+      return;
+    }
+
+    const confirmacion = confirm(`¿Deseas eliminar el equipo "${nombre}"?`);
+    if (!confirmacion) return;
+
+    try {
+      await eliminarEquipo(nombre);
+      console.log(`Equipo "${nombre}" eliminado correctamente.`);
+      alert(`Equipo "${nombre}" eliminado correctamente.`);
+      onUpdate(); // Actualizar listado
+    } catch (error) {
+      console.error(`Error al eliminar el equipo "${nombre}":`, error);
+      alert(`Error al eliminar el equipo "${nombre}".`);
+    }
+  };
+
+  if (!data && !accionCrear) {
+    return <p>Los datos del equipo no están disponibles.</p>;
+  }
+
+  return (
+    <form
+      className="flex flex-col p-5 bg-gray-100 shadow m-2 w-[400px] mx-auto m-2"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <h1 className="text-xl m-auto my-3">{accionCrear ? "Nuevo Equipo" : "Editar Equipo"}</h1>
+
+      <InputText
+        campo="Nombre del equipo"
+        placeholder="Ej: Barcelona FC"
+        type="text"
+        nombre="nombre"
+        register={register}
+        setValue={setValue}
+        errors={errors.nombre}
+        valor={data ? data.nombre : ""}
+        require
+      />
+
+      <div className="mt-2">
+        <label className="block font-semibold" htmlFor="dni_dt_fk">
+          DNI del Director Técnico
+        </label>
+        <select
+          id="dni_dt_fk"
+          {...register("dni_dt_fk", { required: "Seleccione un DNI de DT" })}
+          className={`block text-black w-full p-3 border border-gray-300 rounded-lg shadow-sm ${
+            errors.dni_dt_fk ? "border-red-500" : ""
+          }`}
+        >
+          {listaDnis.map((dni, index) => (
+            <option key={index} value={dni}>
+              {dni}
+            </option>
+          ))}
+        </select>
+        {errors.dni_dt_fk && <p className="text-red-500 text-xs italic">{errors.dni_dt_fk.message}</p>}
+      </div>
+
+      <div className="mb-4 mt-2">
+        <label className="block font-semibold" htmlFor="division">
+          División
+        </label>
+        <select
+          id="division"
+          {...register("division", { required: "Seleccione una división" })}
+          className={`block text-black w-full p-3 border border-gray-300 rounded-lg shadow-sm ${
+            errors.division ? "border-red-500" : ""
+          }`}
+        >
+          {listaDivisiones.map((division, index) => (
+            <option key={index} value={division}>
+              {division}
+            </option>
+          ))}
+        </select>
+        {errors.division && <p className="text-red-500 text-xs italic">{errors.division.message}</p>}
+      </div>
+
+      <div className="mt-2">
+        <label className="block font-semibold" htmlFor="categoria_fk">
+          Categoría
+        </label>
+        <select
+          id="categoria_fk"
+          {...register("categoria_fk", { required: "Seleccione una categoría" })}
+          className={`block text-black w-full p-3 border border-gray-300 rounded-lg shadow-sm ${
+            errors.categoria_fk ? "border-red-500" : ""
+          }`}
+        >
+          {listaCategorias.map((categoria, index) => (
+            <option key={index} value={categoria}>
+              {categoria}
+            </option>
+          ))}
+        </select>
+        {errors.categoria_fk && (
+          <p className="text-red-500 text-xs italic">{errors.categoria_fk.message}</p>
+        )}
+      </div>
+
+      <div className="flex">
+        {accionCrear ? (
+          <button
+            onClick={() => setAccionCrear(true)}
+            className="mr-2 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full my-2"
+          >
+            Crear
+          </button>
+        ) : (
+          <>
+            <button
+              onClick={() => setAccionCrear(false)}
+              className="mr-2 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded w-1/2 my-2"
+            >
+              Editar
+            </button>
+            <button
+              onClick={() => handleEliminar(data?.nombre || "")}
+              className="mr-2 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded w-1/2 my-2"
+            >
+              Eliminar
+            </button>
+          </>
+        )}
+      </div>
+    </form>
+  );
+}
--- src/app/equipos/page.tsx
+++ src/app/equipos/page.tsx
@@ -1,172 +1,71 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import { obtenerEquipos, crearEquipo } from '../../utils/equipos/equipos';
-import DataTable from 'react-data-table-component';
-import ColumnCategorias from './columnEquipos';
-import expandedComponents from './expandedEquipo';
-import { useForm } from 'react-hook-form';
-import InputText from '@/components/Inputs/InputText';
+import React, { useEffect, useState } from "react";
+import { obtenerEquipos } from "../../utils/equipos/equipos";
+import DataTable from "react-data-table-component";
+import ColumnCategorias from "./columnEquipos";
+import EquipoForm from "./EquipoForm";
 
 interface Equipo {
-    nombre: string;
-    dni_dt_fk: number;
-    categoria_fk: string;
-    division: string;
-  }
+  nro_equipo?: number;
+  nombre: string;
+  dni_dt_fk: number;
+  categoria_fk: string;
+  division: string;
+}
 
-export default function ListaEquipos(){
+export default function ListaEquipos() {
+  const [equipos, setEquipos] = useState<Equipo[]>([]);
+  const [vistaAgregar, setVistaAgregar] = useState(false);
 
-    const [equipos, setEquipos] = useState<Equipo[]>([]);
-    const [vistaAgregar, setVistaAgregar] = useState(false);
+  const fetchEquipos = async () => {
+    const equipos = await obtenerEquipos();
+    setEquipos(equipos);
+  };
 
-    const { register, handleSubmit, setValue, formState: { errors } } = useForm();
+  // Cargar equipos al cargar el componente
+  useEffect(() => {
+    fetchEquipos();
+  }, []);
 
-    useEffect(() => {
-        const fetchEquipos = async () => {
-        const equipos = await obtenerEquipos();
-        setEquipos(equipos);
-        };
+  return (
+    <div>
+      <div className="flex p-4">
+        <h1 className="text-3xl mr-2">Listado de equipos</h1>
+        <button
+          className="bg-blue-200 px-4 py-2 rounded-xl"
+          onClick={() => setVistaAgregar(!vistaAgregar)}
+        >
+          Agregar
+        </button>
+      </div>
 
-        fetchEquipos();
-    }, []);
-
-    function cancelarEnvio(){
-        setVistaAgregar(false)
-      }
-
-      const onSubmit = async (values: any) => {
-        
-        console.log(values);
-        values.dni_dt_fk = Number(values.dni_dt_fk);
-        await crearEquipo(values);
-            
-      };
-
-    return(
-
-        <div>
-            <div className='flex p-4'>
-                <h1 className='text-3xl mr-2'>Listado de equipos</h1>
-                <button
-                    className="bg-blue-200 px-4 py-2 rounded-xl"
-                    onClick={() => setVistaAgregar(!vistaAgregar)}
-                >
-                    Agregar
-                </button>
-            </div>
-
-            {vistaAgregar && (
-                <div className="flex items-center justify-center">
-                    <form
-                    className="flex flex-col p-5 bg-gray-100 shadow m-2 w-[400px]"
-                    onSubmit={handleSubmit(onSubmit)}
-                    >
-                    {/* Input para el nombre del equipo */}
-                    <InputText
-                      campo="Nombre del equipo"
-                      placeholder="Ej: Barcelona FC"
-                      type="text"
-                      nombre="nombre"
-                      register={register}
-                      setValue={setValue}
-                      errors={errors.nombre}
-                      require
-                    />
-                    {/* Select para elegir la división */}
-                   <div className="mt-2">
-                      <label className="block font-semibold" htmlFor="dni_dt_fk">
-                        DNI del Director Tecnico
-                      </label>
-                      <select
-                        id="dni_dt_fk"
-                        {...register("dni_dt_fk", { required: "Seleccione un dni de DT" })}
-                        className={`block text-black w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-900 ${
-                          errors.dni_dt_fk ? 'mt-1 text-red-600' : ''
-                        }`}
-                      >
-                        <option value="1345">1345</option>
-                        <option value="12345678">12345678</option>
-                        <option value="66666666">66666666</option>
-                      </select>
-                      {errors.dni_dt_fk && (
-                        <p className="text-red-500 text-xs italic">{errors.dni_dt_fk.message}</p>
-                      )}
-                    </div>
-                    
-                    {/* Select para elegir la división */}
-                    <div className="mb-4 mt-2">
-                      <label className="block font-semibold" htmlFor="division">
-                        División
-                      </label>
-                      <select
-                        id="division"
-                        {...register("division", { required: "Seleccione una división" })}
-                        className={`block text-black w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-900  ${
-                          errors.division ? 'border-red-500' : ''
-                        }`}
-                      >
-                        <option value="A">División A</option>
-                        <option value="B">División B</option>
-                        <option value="C">División C</option>
-                      </select>
-                      {errors.division && (
-                        <p className="text-red-500 text-xs italic">{errors.division.message}</p>
-                      )}
-                    </div>
-
-                    {/* Select para elegir la división */}
-                    <div className="mt-2">
-                        <label className="block font-semibold" htmlFor="categoria_fk">
-                          Categoria
-                        </label>
-                        <select
-                          id="categoria_fk"
-                          {...register("categoria_fk", { required: "Seleccione una categoria" })}
-                          className={`block text-black w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-900  ${
-                            errors.categoria_fk ? 'border-red-500' : ''
-                          }`}
-                        >
-                          <option value="juvenil">juvenil</option>
-                          <option value="Maxi">Maxi</option>
-                          <option value="Super">Super</option>
-                        </select>
-                        {errors.categoria_fk && (
-                          <p className="text-red-500 text-xs italic">{errors.categoria_fk.message}</p>
-                        )}
-                    </div>
-
-
-                    <div className='flex'>
-                        <button className="mr-2 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full my-2">
-                        Crear
-                        </button>
-                        <button className="mr-2 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded w-full my-2" onClick={cancelarEnvio}>
-                        Cancelar
-                        </button>
-                    </div>
-
-                    
-                    </form>
-                </div>
-                )}
-
-
-
-            <DataTable
-                columns={ColumnCategorias}
-                data={equipos}
-                // pagination
-                expandableRows
-                expandableRowsComponent={expandedComponents}
-                responsive
-                striped
-                highlightOnHover
-                noDataComponent="No hay equipos para mostrar"
-            />
+      {vistaAgregar && (
+        <div className="flex items-center justify-center">
+          <EquipoForm
+            creando={true}
+            data={undefined}
+            onUpdate={fetchEquipos} // Actualiza la lista tras crear o modificar un equipo
+          />
         </div>
+      )}
 
-        
-
-    );
+      <DataTable
+        columns={ColumnCategorias}
+        data={equipos}
+        expandableRows
+        expandableRowsComponent={({ data }) => (
+          <EquipoForm
+            creando={false}
+            data={data}
+            onUpdate={fetchEquipos} // Actualiza la lista tras eliminar o editar un equipo
+          />
+        )}
+        responsive
+        striped
+        highlightOnHover
+        noDataComponent="No hay equipos para mostrar"
+      />
+    </div>
+  );
 }
